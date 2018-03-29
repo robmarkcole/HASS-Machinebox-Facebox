e0060a47a132d311f6d8a39548b9dda005f01870
robmarkcole <robmarkcole@gmail.com>
2018-03-29 07:06:31 +0100

    Working v0.1

--- custom_components/image_processing/facebox.py
+++ custom_components/image_processing/facebox.py
@@ -6,6 +6,7 @@ https://home-assistant.io/components/image_processing.facebox
 """
 import base64
 from datetime import timedelta
+import io
 import requests
 import logging
 import voluptuous as vol
@@ -18,16 +19,12 @@ from homeassistant.components.image_processing import (
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_URL = 'url'
-CONF_CONCEPTS = 'concepts'
-DEFAULT_CONCEPTS = 'NO_CONCEPT'
+CONF_ENDPOINT = 'endpoint'
 
-SCAN_INTERVAL = timedelta(seconds=5)
+SCAN_INTERVAL = timedelta(seconds=8)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_URL): cv.string,
-    vol.Optional(CONF_CONCEPTS, default=[DEFAULT_CONCEPTS]):
-        vol.All(cv.ensure_list, [cv.string]),
+    vol.Required(CONF_ENDPOINT): cv.string,
 })
 
 
@@ -35,64 +32,58 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the classifier."""
     entities = []
     for camera in config[CONF_SOURCE]:
-        entities.append(Classifier(
+        entities.append(Facebox(
             hass,
             camera.get(CONF_NAME),
-            config[CONF_URL],
-            config[CONF_CONCEPTS],
+            config[CONF_ENDPOINT],
             camera[CONF_ENTITY_ID],
         ))
     add_devices(entities)
 
 
-class Classifier(ImageProcessingEntity):
-    """Perform a classification via a rest API."""
+class Facebox(ImageProcessingEntity):
+    """Perform a classification via a Facebox."""
 
     ICON = 'mdi:file'
 
-    def __init__(self, hass, name, url, concepts, camera_entity):
+    def __init__(self, hass, name, endpoint, camera_entity):
         """Init with the API key and model id"""
         self.hass = hass
         if name:  # Since name is optional.
             self._name = name
         else:
-            self._name = "Classifier {0}".format(
+            self._name = "Facebox {0}".format(
                 split_entity_id(camera_entity)[1])
-        self._concepts = concepts
         self._camera_entity = camera_entity
-        self._headers = {'content-type': 'application/json, charset=utf-8'}
-        self._url = url
-        self._classifications = {}  # The dict of classifications
-        self._state = None    # The most likely classification
+    #    self._headers = {'content-type': 'application/json, charset=utf-8'}
+        self._url = "http://{}/facebox/check".format(endpoint)
+        self._state = None
 
     def process_image(self, image):
-        """Perform classification of a single image."""
-        base64_img = base64.b64encode(image).decode('ascii')
-        json_data = {"base64": base64_img}
+        """Process an image."""
         response = requests.post(
-            self._url, headers=self._headers, json=json_data).json()
+            self._url,
+            json=self.encode_image(image)
+            ).json()
 
         if response['success']:
-            data = response['tags']
-            self._classifications = {item['tag']: round(
-                100.0*item['confidence'], 1) for item in data}
-            #_LOGGER.error("classifications %s", self._classifications)
-
-            self._state = next(iter(self._classifications))
-            self.fire_concept_events()
+            self._state = response['facesCount']
         else:
             self._state = "Request_failed"
-            self._classifications = {}
 
-    def fire_concept_events(self):
-        """Fire an event for each concept identified."""
-        identified_concepts = self._classifications.keys() & self._concepts
-        for concept in identified_concepts:
-            self.hass.bus.fire(
-                'Classifier', {
-                    CONF_ENTITY_ID: self._camera_entity,
-                    'concept': concept,
-                    })
+    def encode_image(self, image):
+        """base64 encode an image stream."""
+        base64_img = base64.b64encode(image).decode('ascii')
+        return {"base64": base64_img}
+
+    def save_image(self, image):
+        """Save an image stream and return the filename."""
+        from PIL import Image
+        stream = io.BytesIO(image)
+        img = Image.open(stream)
+        IMG = 'facebox.jpg'
+        img.save(IMG)
+        return IMG
 
     @property
     def device_class(self):
@@ -112,7 +103,7 @@ class Classifier(ImageProcessingEntity):
     @property
     def state_attributes(self):
         """Return device specific state attributes."""
-        attr = self._classifications
+        attr = {}
         return attr
 
     @property
